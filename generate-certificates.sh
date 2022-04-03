d1e9b888cd6c01c79890ef3cb9ca2ed90bf7ee93
Tim Ritter <timrittermail@gmail.com>
2022-04-03 15:27:18 +0200

    Remove old certificate generation script

--- generate-certificates.sh
+++ generate-certificates.sh
@@ -3,6 +3,8 @@
 set -e
 cd "$(dirname "$0")"
 
+echo "This script requires openssl. To install it use \"sudo apt-get -y install openssl\" or \"sudo dnf -y install openssl depending\" on your Linux distribution."
+
 {
     mkdir -p ./data/certificates
 } || {
